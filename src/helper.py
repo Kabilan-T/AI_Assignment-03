f6683d44ca68f237284aa2ce08487174370eeff1
kabilan <kavikabilan37@gmail.com>
2020-11-26 11:30:05 +0530

    Question

---
+++ src/helper.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 01:36:01 2018
+
+@author: Iswariya Manivannan
+"""
+
+import sys
+import os
+import time
+
+
+def maze_map_to_tree(maze_map):
+    """Function to create a tree from the map file. The idea is
+    to check for the possible movements from each position on the
+    map and encode it in a data structure like list.
+
+    Parameters
+    ----------
+    maze_map : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+
+    raise NotImplementedError
+
+
+def assign_character_for_nodes(maze_map, current_node, prev_node):
+    """Function to assign character for the visited nodes. Please assign
+    meaningful characters based on the direction of tree traversal.
+
+    Parameters
+    ----------
+    maze_map : [type]
+        [description]
+    current_node : [type]
+        [description]
+    prev_node : [type]
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+
+
+    raise NotImplementedError
+
+
+def write_to_file(file_name, path):
+    """Function to write output to console and the optimal path
+    from start to each goal to txt file.
+    Please ensure that it should ALSO be possible to visualize each and every
+    step of the tree traversal algorithm in the map in the console.
+    This enables understanding towards the working of your
+    tree traversal algorithm as to how it reaches the goals.
+
+    Parameters
+    ----------
+    filen_name : string
+        This parameter defines the name of the txt file.
+    path : [type]
+        [description]
+
+    """
+
+    raise NotImplementedError
